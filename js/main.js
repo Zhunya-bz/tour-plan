e5adb0058548c6a89904f780edfc76618243a411
Zhunya-bz <fluffylion2013@gmail.com>
2020-11-24 11:56:49 +0700

    create: added slider-swiper in hotel section

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
+
+  // If we need pagination это точки внизу
+ /* pagination: {
+    el: '.swiper-pagination',
+  },*/
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button_next',
+    prevEl: '.slider-button_prev',
+  },
+
+  // And if we need scrollbar
+  /*scrollbar: {
+    el: '.swiper-scrollbar',
+  },*/
+})
