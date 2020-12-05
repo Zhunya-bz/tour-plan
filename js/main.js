6f8178b0ce0cd5ae8cef5ae18955440459ac25a8
Zhunya-bz <fluffylion2013@gmail.com>
2020-12-05 14:00:09 +0700

    create: php server and add modal window with Esc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSwiper = new Swiper('#hotel-slider', {
+$(document).ready(function(){
+  var hotelSwiper = new Swiper('#hotel-slider', {
   // Optional parameters
   //direction: 'vertical',
   loop: true,
@@ -19,23 +20,20 @@ var hotelSwiper = new Swiper('#hotel-slider', {
   grabCursor: true,
   //touchAngle: 45,
   //watchSlidesProgress: true,
-
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-
   // And if we need scrollbar
   /*scrollbar: {
     el: '.swiper-scrollbar',
   },*/
-})
+});
 
 var mySwiper = new Swiper('.reviews-slider', {
   // Optional parameters
   //direction: 'vertical',
   loop: true,
-
   // Navigation arrows
   navigation: {
     nextEl: '.reviews-slider__button_next',
@@ -49,11 +47,42 @@ var mySwiper = new Swiper('.reviews-slider', {
     onlyInViewport: false,
   },
   autoHeight: true,
-})
+});
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function(){
   //console.log("клик по кнопке меню");
   document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom_active');
 }
-)
+);
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $('.modal__close');
+
+//открытие модального окна по клику на кнопку
+modalButton.on('click', openModal);
+
+//нажатие на клавишу Esc
+$(document).on('keydown', function(evt) {
+  if (evt.code == "Escape") {     //(e.keyCode == 27){ 
+    //console.log('ты нажала на клавишу esc');
+    closeModal(evt);}
+});
+//закрытие по клику на крестик
+closeModalButton.on('click', closeModal);
+
+function openModal(){
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.addClass('modal__overlay_visible');
+  modalDialog.addClass('modal__dialog_visible');
+}
+
+function closeModal(event){
+  event.preventDefault();
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay_visible');
+  modalDialog.removeClass('modal__dialog_visible');
+}
+})
